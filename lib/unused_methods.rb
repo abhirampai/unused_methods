3253a09c560ea070290da9ad647deff9c5d19a46
Abhiram Pai <abhirampai1999@gmail.com>
2024-08-09 18:21:24 +0530

    Added prefix method to unused methods checker

--- lib/unused_methods.rb
+++ lib/unused_methods.rb
@@ -5,9 +5,5 @@ require_relative "unused_methods/unused_method_checker"
 require_relative "unused_methods/cli"
 
 module UnusedMethods
-  def self.prefix
-    "vscode://file/"
-  end
-
   class Error < StandardError; end
 end
