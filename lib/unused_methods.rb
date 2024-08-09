0df3e1f89d41ee122bea474746126cda926add1f
Abhiram Pai <abhirampai1999@gmail.com>
2024-08-09 17:53:28 +0530

    move attr_accessor to inside of self method

--- lib/unused_methods.rb
+++ lib/unused_methods.rb
@@ -5,6 +5,8 @@ require_relative "unused_methods/unused_method_checker"
 require_relative "unused_methods/cli"
 
 module UnusedMethods
-  thread_mattr_accessor :prefix, default: "vscode://file/"
+  class << self
+    attr_accessor :prefix, default: "vscode://file/"
+  end
   class Error < StandardError; end
 end
