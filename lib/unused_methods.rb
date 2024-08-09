534ee7dd2acbd7dbef2990142ed8a184dde617c5
Abhiram Pai <abhirampai1999@gmail.com>
2024-08-09 18:04:50 +0530

    Changed prefix to be a method in unused methods

--- lib/unused_methods.rb
+++ lib/unused_methods.rb
@@ -5,6 +5,9 @@ require_relative "unused_methods/unused_method_checker"
 require_relative "unused_methods/cli"
 
 module UnusedMethods
-  thread_mattr_accessor :prefix, default: "vscode://file/"
+  def self.prefix
+    "vscode://file/"
+  end
+
   class Error < StandardError; end
 end
